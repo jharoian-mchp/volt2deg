47fee41e7889349b3a76874ca1800409ef21b2d9
John Haroian <john.haroian@microchip.com>
2025-08-20 15:55:52 -0700

    Added calls to test 7Seg.c

--- main.c
+++ main.c
@@ -4,12 +4,20 @@
  * @date 2025-08-20
  * @brief Main function
  */
- #include "My_MCC_Config/mcc/mcc_generated_files/system/system.h"
 
-  int main(void)
+#include "My_MCC_Config/mcc/mcc_generated_files/system/system.h"
+#include "7seg.h"
+
+ int main(void)
  {
      SYSTEM_Initialize();
- 
+
+     seven_seg_init();
+
+     seven_seg_clear();
+
+     seven_seg_display_digit(8);
+
      while(1)
      {
      }    
