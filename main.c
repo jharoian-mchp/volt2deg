b1d253e2fa33d2931e8fbdb67d4068a3d758a204
John Haroian <john.haroian@microchip.com>
2025-08-20 13:53:41 -0700

    Added initial main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+/**
+ * @file main.c
+ * @author c14029
+ * @date 2025-08-20
+ * @brief Main function
+ */
+int main(){
+
+    // Add your code here and press Ctrl + Shift + B to build
+    while(1);
+    return 0;
+}
