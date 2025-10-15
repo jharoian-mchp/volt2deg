631c2ff3cfa5c6eb5ebd1f9b456259961e51522a
John Haroian <john.haroian@microchip.com>
2025-10-15 16:59:49 -0700

    New MPLAB project used for MCC to generate BSP

---
+++ mcc-project/main.c
@@ -0,0 +1,12 @@
+/**
+ * @file main.c
+ * @author c14029
+ * @date 2025-10-15
+ * @brief Main function
+ */
+int main(){
+
+    // Add your code here and press Ctrl + Shift + B to build
+
+    return 0;
+}
